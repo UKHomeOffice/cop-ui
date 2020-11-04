48181b47c97a830317317fb42438edcd0b39143f
JenniKate Wallace <jennikate@gmail.com>
2020-11-04 16:28:47 +0000

    Add case titles & search
    Add search box (non-functioning)

--- client/src/pages/cases/CasePage.jsx
+++ client/src/pages/cases/CasePage.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import './CasesPage.scss';
 
 const CasePage = () => {
-  return <></>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <span className="govuk-caption-l">{t('pages.cases.caption')}</span>
+          <h1 className="govuk-heading-l">{t('pages.cases.heading')}</h1>
+          <div className="govuk-inset-text">
+            <p>{t('pages.cases.heading-help')}</p>
+            <p>
+              <strong>{t('pages.cases.heading-warning')}</strong>
+            </p>
+          </div>
+        </div>
+        <div className="govuk-grid-column-one-third">
+          <div className="govuk-form-group">
+            <input
+              // onChange={(e) => {
+              //   search(e.target.value);
+              // }}
+              spellCheck="false"
+              className="govuk-input search__input"
+              placeholder={t('pages.cases.list.search-placeholder')}
+              id="bfNumber"
+              name="search"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CasePage;
