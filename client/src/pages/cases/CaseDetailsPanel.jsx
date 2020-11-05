5a940d54f9e978d3dfc598d55ea1bb347730f668
JenniKate Wallace <jennikate@gmail.com>
2020-11-05 17:24:04 +0000

    Add accordion section

--- client/src/pages/cases/CaseDetailsPanel.jsx
+++ client/src/pages/cases/CaseDetailsPanel.jsx
@@ -28,11 +28,26 @@ const CaseDetailsPanel = () => {
           <div className="govuk-form-group">
             <label className="govuk-label" htmlFor="sort">
               Order by
-              <select className="govuk-select" id="sort" name="sort">
+              <select
+                className="govuk-select govuk-!-display-block govuk-!-margin-top-1"
+                id="sort"
+                name="sort"
+              >
                 <option value="desc">Latest process start date</option>
                 <option value="acs">Earliest process start date</option>
               </select>
             </label>
+          </div>
+          <div id="businessKey" className="govuk-accordion" data-module="govuk-accordion">
+            <div className="govuk-accordion__section">
+              <div className="govuk-accordion__section-header">
+                <h4 className="govuk-accordion__section-heading">
+                  <span className="govuk-accordion__section-button" id="id">
+                    example title
+                  </span>
+                </h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
