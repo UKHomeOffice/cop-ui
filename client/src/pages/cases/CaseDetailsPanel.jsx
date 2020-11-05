1c7542cc367b99faba0fbd4f7fcf82179f69d73f
JenniKate Wallace <jennikate@gmail.com>
2020-11-05 17:01:18 +0000

    Add detail rows

--- client/src/pages/cases/CaseDetailsPanel.jsx
+++ client/src/pages/cases/CaseDetailsPanel.jsx
@@ -17,6 +17,35 @@ const CaseDetailsPanel = () => {
           </button>
         </div>
       </div>
+      <div className="govuk-grid-row govuk-card govuk-!-margin-top-4">
+        <div className="govuk-grid-column-full">
+          <h3 className="govuk-heading-m">Case actions</h3>
+        </div>
+      </div>
+      <div className="govuk-grid-row govuk-card govuk-!-margin-top-4">
+        <div className="govuk-grid-column-full">
+          <h3 className="govuk-heading-m">Case history</h3>
+          <div className="govuk-form-group">
+            <label className="govuk-label" htmlFor="sort">
+              Order by
+              <select className="govuk-select" id="sort" name="sort">
+                <option value="desc">Latest process start date</option>
+                <option value="acs">Earliest process start date</option>
+              </select>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="govuk-grid-row govuk-card govuk-!-margin-top-4">
+        <div className="govuk-grid-column-full">
+          <h3 className="govuk-heading-m">Case attachments</h3>
+        </div>
+      </div>
+      <div className="govuk-grid-row govuk-card govuk-!-margin-top-4">
+        <div className="govuk-grid-column-full">
+          <h3 className="govuk-heading-m">Case metrics</h3>
+        </div>
+      </div>
     </>
   );
 };
