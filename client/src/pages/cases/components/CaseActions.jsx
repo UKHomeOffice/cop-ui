bdebf2a44b1ac59fd8f75fa26b3ed19c3c90957a
JenniKate Wallace <jennikate@gmail.com>
2020-11-06 10:17:20 +0000

    Move sections out to their own components

---
+++ client/src/pages/cases/components/CaseActions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CaseActions = () => {
+  return (
+    <>
+      <div className="govuk-grid-column-full">
+        <h3 className="govuk-heading-m">Case actions</h3>
+      </div>
+    </>
+  );
+};
+
+export default CaseActions;
---
+++ client/src/pages/cases/components/CaseMetrics.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CaseMetrics = () => {
+  return (
+    <>
+      <div className="govuk-grid-column-full">
+        <h3 className="govuk-heading-m">Case metrics</h3>
+      </div>
+    </>
+  );
+};
+
+export default CaseMetrics;
