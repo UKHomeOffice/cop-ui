bca1744f0a05bbde8670b602caec531aff100751
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2020-11-10 16:37:15 +0000

    Add NotFound error handler and test files

---
+++ client/src/components/NotFound.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotFound from './NotFound';
+
+describe('Not Found page', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<NotFound />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
