16c8031ea3f25dda6b93d3294c35b63e9630f3da
Gavin Orland <gavin.orland@gmail.com>
2020-11-17 17:46:30 +0000

    Add route for reports

--- client/src/pages/reports/routes.js
+++ client/src/pages/reports/routes.js
@@ -2,6 +2,7 @@ import { map, mount, route } from 'navi';
 import React from 'react';
 import { withAuthentication } from '../../routes/utils';
 import ReportsListPage from './ReportsListPage';
+import PowerBIReport from './components/PowerBIReport';
 
 const routes = mount({
   '/': map((request, context) =>
@@ -12,6 +13,15 @@ const routes = mount({
       })
     )
   ),
+  '/:reportName': map((request, context) =>
+  withAuthentication(
+    route({
+      title: context.t('pages.report.title'),
+      view: <PowerBIReport />,
+    })
+  )
+),
+
 });
 
 export default routes;
