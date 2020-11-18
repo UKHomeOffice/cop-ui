d89489b5906170442303816b611675e9202aaecd
Gavin Orland <gavin.orland@gmail.com>
2020-11-18 20:03:05 +0000

    Fetch user's team when application loads

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, Suspense } from 'react';
+import axios from 'axios';
 import './App.scss';
 import { Router, View } from 'react-navi';
 import HelmetProvider from 'react-navi-helmet';
@@ -10,6 +11,7 @@ import { initAll } from 'govuk-frontend';
 import Layout from './components/layout';
 import routes from './routes';
 import ApplicationSpinner from './components/ApplicationSpinner';
+import { useAxios, useIsMounted } from './utils/hooks';
 import { TeamContext } from './utils/TeamContext';
 
 if (window.ENVIRONMENT_CONFIG) {
@@ -42,12 +44,51 @@ const keycloakProviderInitConfig = {
 const RouterView = () => {
   const { t } = useTranslation();
   const [keycloak, initialized] = useKeycloak();
-  if (!initialized) {
-    return <ApplicationSpinner translationKey="keycloak.initialising" />;
-  }
+
+  const axiosInstance = useAxios();
+  const isMounted = useIsMounted();
   const { setTeam } = useContext(TeamContext);
+
   initAll();
-  return (
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    if (initialized) {
+      const {
+        tokenParsed: { team_id: teamid },
+      } = keycloak;
+      const fetchData = async () => {
+        if (axiosInstance) {
+          try {
+            if (teamid) {
+              const response = await axiosInstance.get(
+                `/refdata/v2/entities/team?filter=id=eq.${teamid}`,
+                {
+                  cancelToken: source.token,
+                }
+              );
+              if (isMounted.current) {
+                const [team] = response.data.data;
+                setTeam(team);
+              }
+            } else {
+              // TODO: Redirect the user here because they have no teamid in KC...
+            }
+          } catch (error) {
+            if (isMounted.current) {
+              setTeam({});
+            }
+          }
+        }
+      };
+      fetchData();
+    }
+    return () => {
+      source.cancel('Cancelling request');
+    };
+  }, [axiosInstance, initialized, isMounted, keycloak, setTeam]);
+
+  return initialized ? (
     <Router
       hashScrollBehavior="smooth"
       routes={routes}
@@ -57,6 +98,8 @@ const RouterView = () => {
         <View />
       </Layout>
     </Router>
+  ) : (
+    <ApplicationSpinner translationKey="keycloak.initialising" />
   );
 };
 const App = () => (
