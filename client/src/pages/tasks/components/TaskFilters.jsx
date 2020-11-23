85496acfc4b2f02ee8ad4f328e948ffdcf5d1170
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2020-11-23 13:10:58 +0000

    Reformat taskfilter component

--- client/src/pages/tasks/components/TaskFilters.jsx
+++ client/src/pages/tasks/components/TaskFilters.jsx
@@ -7,44 +7,44 @@ const TaskFilters = ({ search, handleFilters }) => {
       <div className="govuk-grid-column-one-third">
         <div className="govuk-form-group">
           <label className="govuk-label" htmlFor="sort">
-            Sort by
-            <select className="govuk-select" id="sort" name="sort" onChange={handleFilters}>
-              <option value="oldest-due-date">Oldest due date</option>
-              <option value="latest-due-date">Latest due date</option>
-              <option value="oldest-created-date">Oldest created date</option>
-              <option value="latest-created-date">Latest created date</option>
-              <option value="highest-priority">Highest priority</option>
-              <option value="lowest-priority">Lowest priority</option>
-            </select>
+            Sort by:
           </label>
+          <select className="govuk-select" id="sort" name="sort" onChange={handleFilters}>
+            <option value="oldest-due-date">Oldest due date</option>
+            <option value="latest-due-date">Latest due date</option>
+            <option value="oldest-created-date">Oldest created date</option>
+            <option value="latest-created-date">Latest created date</option>
+            <option value="highest-priority">Highest priority</option>
+            <option value="lowest-priority">Lowest priority</option>
+          </select>
         </div>
       </div>
       <div className="govuk-grid-column-one-third">
         <div className="govuk-form-group">
           <label className="govuk-label" htmlFor="group">
-            Group by
-            <select className="govuk-select" id="group" name="groupBy" onChange={handleFilters}>
-              <option value="category">Category</option>
-              <option value="bf-reference">BF Reference</option>
-              <option value="priority">Priority</option>
-              <option value="assignee">Assignee</option>
-            </select>
+            Group by:
           </label>
+          <select className="govuk-select" id="group" name="groupBy" onChange={handleFilters}>
+            <option value="category">Category</option>
+            <option value="bf-reference">BF Reference</option>
+            <option value="priority">Priority</option>
+            <option value="assignee">Assignee</option>
+          </select>
         </div>
       </div>
       <div className="govuk-grid-column-one-third">
         <div className="govuk-form-group">
           <label className="govuk-label" htmlFor="filterTaskName">
             Search by task name:
-            <input
-              className="govuk-input govuk-!-width-full"
-              id="filterTaskName"
-              type="text"
-              name="search"
-              value={search}
-              onChange={handleFilters}
-            />
           </label>
+          <input
+            className="govuk-input govuk-!-width-full"
+            id="filterTaskName"
+            type="text"
+            name="search"
+            value={search}
+            onChange={handleFilters}
+          />
         </div>
       </div>
     </div>
