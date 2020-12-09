709e76317e4f3071fae84186d8790abfe65ab1cd
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2020-12-09 09:43:11 +0000

    Configure secure local storage

---
+++ client/src/utils/SecureLocalStorage.js
@@ -0,0 +1,9 @@
+import SecureLS from 'secure-ls';
+
+const secureLocalStorage = new SecureLS({
+  encodingType: 'aes',
+  encryptionSecret: process.env.WWW_STORAGE_KEY,
+  isCompression: true,
+});
+
+export default secureLocalStorage;
